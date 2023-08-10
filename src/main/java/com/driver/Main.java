443f1a4292da69d8e5e09e2c8ee0c74011147483
Shivraj42 <shivraj.tapkire@gmail.com>
2023-08-10 12:02:46 +0530

    attempt_2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,10 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B obj= new B();
-        System.out.println(obj.meth());
+        A objA = new A();
+        System.out.println(objA.meth());
+        B objB= new B();
+        System.out.println(objB.meth());
     }
   
 }
